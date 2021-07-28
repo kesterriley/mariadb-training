4d490f9d63390fd67e456d1bd687c5523626ae18
kesterriley <kesterriley@hotmail.com>
2021-07-28 12:38:50 +0100

    Update causal_read_test.sh
    I have updated this script to do a few things, the first change is to make sure the table exists before running the script, and secondly, I have modified the line that inserts the data.
    
    This is possibly not casual reads, as I have set the process to run in the background, which means it may not be finished before the read line executes.
    
    It does highlight the issue that could happen from an application and the Script runs and executes with failures. On three runs I get 287, 331 and 291 Failures out of 1000.
    
    But the failure is really due to the fact the insert is on a separate thread. The Script as it was was always going to complete the insert before moving on to the read. 
    
    Doing some tuning does not help with the failure rate, three runs: 368, 393 and 384.
    
    You can see the & at the end of line 48. This makes it a background thread - technically this should be removed - but then we never get any failures !

--- sample_scripts/causal_read_test.sh
+++ sample_scripts/causal_read_test.sh
@@ -1,15 +1,65 @@
 #!/bin/bash
-PASSWORD="PASSWORD"
+
+######################################################################
+### Simple Bash Script to test Causal Reads over a MariaDB Cluster ###
+###                                                                ###
+### Author: Catherine Trejo <catherine.trejo@mariadb.com>          ###
+### Updated: Kester Riley <kester.riley@mariadb.com>               ###
+### Version 1.1 July 2021                                          ###
+######################################################################
+
+
+######################################################################
+### Make sure you enter the password you set for the labtest user  ###
+######################################################################
+
+PASSWORD="mariadb"
+
+######################################################################
+### This is the internal IP address of your Node 1 server          ###
+######################################################################
+
 NODE1="NODE-ONE-IP-ADDRESS"
+
+######################################################################
+### This is the internal IP address of your Node 2 server          ###
+######################################################################
+
 NODE2="NODE-TWO-IP-ADDRESS"
+
+######################################################################
+# Try replacing with 10000 to increase the amount of itterations   ###
+######################################################################
+
+ITTERATIONS=1000
+
+######################################################################
+### You should not need to change anything below this line         ###
+######################################################################
+
 FAILURES=0
-for i in {1..1000}; do
-echo -n -e "\nRound $i ";
-mariadb -ulabtest -p$PASSWORD -h $NODE1 test -e"INSERT INTO t VALUES($i)";
-ret=$(mariadb --batch -ulabtest -p$PASSWORD -h $NODE2 test -e"SELECT * FROM t WHERE id=$i");
-if [ "$ret" = "" ]; then
-echo -n "FAILED"
-FAILURES=$((FAILURES +1))
-fi
-done
-echo -e "\n${FAILURES} causal read failures per 1000 reads"
+
+function main {
+  test_and_truncate
+  i=1
+  while [[  $i -le $ITTERATIONS ]]
+  do
+    echo -n -e "\nRound $i"
+    mariadb -ulabtest -p$PASSWORD -h $NODE1 test -e"INSERT INTO t VALUES($i)" > /dev/null 2>&1 &
+    ret=$(mariadb --batch -ulabtest -p$PASSWORD -h $NODE2 test -e"SELECT * FROM t WHERE id=$i")
+    if [ "$ret" = "" ]; then
+      echo -n " FAILED"
+      FAILURES=$((FAILURES +1))
+    fi
+    ((i = i + 1))
+  done
+  echo -e "\n${FAILURES} causal read failures per $ITTERATIONS reads"
+}
+
+function test_and_truncate {
+  #Ensure table exists and truncted before running script
+  table_check=`mariadb -ulabtest -p$PASSWORD -h $NODE1 -bse  "SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 't' and TABLE_SCHEMA = 'test'"`
+  if [ "$table_check" == "1" ]; then  echo "Table Exists"; mariadb -ulabtest -p$PASSWORD -h $NODE1 test -e"TRUNCATE test.t" > /dev/null 2>&1; else echo "The tabble does not exist, or can not be connected, please check the instructions."; exit; fi
+}
+
+main
